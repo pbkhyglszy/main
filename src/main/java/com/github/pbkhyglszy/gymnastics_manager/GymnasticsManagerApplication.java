d3b3077f551d3bf5ca6df2c4d7bae041435442a8
WarmthDawn <warmth06@sina.com>
2021-11-01 14:56:44 +0800

    :tada: 新建项目

---
+++ src/main/java/com/github/pbkhyglszy/gymnastics_manager/GymnasticsManagerApplication.java
@@ -0,0 +1,13 @@
+package com.github.pbkhyglszy.gymnastics_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymnasticsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymnasticsManagerApplication.class, args);
+	}
+
+}
