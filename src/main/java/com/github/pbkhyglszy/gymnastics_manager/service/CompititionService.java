776ab4afbd0cc47e33a96fdab847792a851ea2ea
sunday7994 <18086483915@163.com>
2021-11-08 16:02:07 +0800

    :construction:摸，都可以摸

---
+++ src/main/java/com/github/pbkhyglszy/gymnastics_manager/service/CompititionService.java
@@ -0,0 +1,4 @@
+package com.github.pbkhyglszy.gymnastics_manager.service;
+
+public class CompititionService {//录入比赛
+}
---
+++ src/main/java/com/github/pbkhyglszy/gymnastics_manager/service/TeamService.java
@@ -0,0 +1,5 @@
+package com.github.pbkhyglszy.gymnastics_manager.service;
+
+public class TeamService {//设置代表队名称、账号和缺省密码
+
+}
