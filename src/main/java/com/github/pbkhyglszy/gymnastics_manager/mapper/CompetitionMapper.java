7db59ee1b1506dd82e751a37073ec746c346945a
SkyCrystal <pb2001@163.com>
2021-11-22 16:29:26 +0800

    CompetitionService

---
+++ src/main/java/com/github/pbkhyglszy/gymnastics_manager/mapper/CompetitionMapper.java
@@ -0,0 +1,26 @@
+package com.github.pbkhyglszy.gymnastics_manager.mapper;
+
+import com.github.pbkhyglszy.gymnastics_manager.entity.Competition;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.sql.SQLException;
+import java.util.List;
+
+@Mapper
+public interface CompetitionMapper {
+    //    List<Test> getAll();
+    int addNewCompetition(Competition competition) throws SQLException;
+
+    void deleteCompetitionById(int id) throws SQLException;
+
+    void modifyCompetitionById(int id, Competition competition) throws SQLException;
+
+    List<Competition> getCompetitionList();
+
+    Competition getCompetitionById(int id);
+
+    /**
+     根据传入的模式串对象决定筛选条件 若某个属性非NULL 则选择与该属性相同的所有Competition
+     */
+    List<Competition> getCompetitionByCondition(Competition pattern);
+}
--- src/main/java/com/github/pbkhyglszy/gymnastics_manager/service/CompetitionService.java
+++ src/main/java/com/github/pbkhyglszy/gymnastics_manager/service/CompetitionService.java
@@ -1,24 +1,49 @@
 package com.github.pbkhyglszy.gymnastics_manager.service;
 
 import com.github.pbkhyglszy.gymnastics_manager.entity.Competition;
+import com.github.pbkhyglszy.gymnastics_manager.mapper.CompetitionMapper;
+import com.github.pbkhyglszy.gymnastics_manager.mapper.TestMapper;
+import lombok.SneakyThrows;
+import org.apache.ibatis.jdbc.SQL;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.sql.SQLException;
 import java.util.Collections;
 import java.util.List;
 
+@Service
 public class CompetitionService {//录入比赛，增删改查
-    public void addCompetition(Competition competition){
 
+
+    private CompetitionMapper competitionMapper;
+
+    @Autowired
+    public CompetitionService(CompetitionMapper competitionMapper) {
+        this.competitionMapper = competitionMapper;
     }
 
+
+    @SneakyThrows
+    public int addCompetition(Competition competition) {
+        return competitionMapper.addNewCompetition(competition);
+    }
+    @SneakyThrows
     public void deleteCompetition(int competitionId){
-
+        competitionMapper.deleteCompetitionById(competitionId);
     }
-
+    @SneakyThrows
     public void updateCompetition(Competition competition){
-
+        competitionMapper.modifyCompetitionById(competition.getId(),competition);
     }
-
+    @SneakyThrows
     public List<Competition> getCompetitions(){
-        return Collections.emptyList();
+        return competitionMapper.getCompetitionList();
+    }
+    public Competition getCompetition(int id){
+        return competitionMapper.getCompetitionById(id);
+    }
+    public List<Competition> getCompetitionsByCondition(Competition Pattern){
+        return competitionMapper.getCompetitionByCondition(Pattern);
     }
 }
