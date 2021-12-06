c0b9508754060d7186d21e9a5d507faf51634f12
SkyCrystal <pb2001@163.com>
2021-12-06 16:55:56 +0800

    password generator

--- src/main/java/com/github/pbkhyglszy/gymnastics_manager/mapper/LoginMapper.java
+++ src/main/java/com/github/pbkhyglszy/gymnastics_manager/mapper/LoginMapper.java
@@ -4,6 +4,11 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface LoginMapper {
+    /**
+     * this password is encoded!
+     * @param id id of the user to operate
+     * @param password the encoded password, see LoginService.java
+     */
     void setPassword(int id, String password);
 
     String getPassword(int id);
--- src/main/java/com/github/pbkhyglszy/gymnastics_manager/service/LoginService.java
+++ src/main/java/com/github/pbkhyglszy/gymnastics_manager/service/LoginService.java
@@ -1,35 +1,38 @@
 package com.github.pbkhyglszy.gymnastics_manager.service;
 
 import com.github.pbkhyglszy.gymnastics_manager.mapper.LoginMapper;
-import org.apache.ibatis.annotations.Mapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
+
+import java.util.Base64;
+import java.util.Random;
 
 @Service
 public class LoginService {
     LoginMapper loginMapper;
-    String salt="ThisIsASalt";
+    String salt = "ThisIsASalt";
+    Base64.Encoder b64Encoder = Base64.getEncoder();
+    Random Rand= new Random();
     String generatePassword(String account) {
-        //TODO: random string generator
-        return "1234";
+        byte[] token = new byte[10];
+        Rand.nextBytes(token);
+        return b64Encoder.encodeToString(token);
     }
 
-    void setPassword(int id,String password)
-    {
-        loginMapper.setPassword(id,password);
+    void setPassword(int id, String password) {
+        loginMapper.setPassword(id, DigestUtils.md5DigestAsHex((password + salt).getBytes()));
     }
 
-    boolean validatePassword(int id,String password)
-    {
-        password=DigestUtils.md5DigestAsHex((password+salt).getBytes());
-        String storedPassword= loginMapper.getPassword(id);
+    boolean validatePassword(int id, String password) {
+        password = DigestUtils.md5DigestAsHex((password + salt).getBytes());
+        String storedPassword = loginMapper.getPassword(id);
         return storedPassword.equals(password);
     }
-
-    String generateToken(int Userid)
-    {
-        //TODO: random string generator
-        return "1234";
+    @Deprecated
+    String generateToken(int Userid) {
+        Random R = new Random(Userid + System.currentTimeMillis() / 1000000);
+        byte[] token = new byte[64];
+        R.nextBytes(token);
+        return b64Encoder.encodeToString(token);
     }
 }
