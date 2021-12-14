73fea45c75ce39113dc722762c862877fdf782d5
sunday7994 <18086483915@163.com>
2021-12-14 19:17:41 +0800

    增加系统状态（开始、结束报名），实现运动员号码生成

--- src/main/java/com/github/pbkhyglszy/gymnastics_manager/mapper/SystemStatusMapper.java
+++ src/main/java/com/github/pbkhyglszy/gymnastics_manager/mapper/SystemStatusMapper.java
@@ -1,4 +1,15 @@
 package com.github.pbkhyglszy.gymnastics_manager.mapper;
 
+import com.github.pbkhyglszy.gymnastics_manager.entity.Athlete;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
 public interface SystemStatusMapper {
+    int update(int i);
+    int get();
+    List<Integer> getMaleAthleteIds();
+    List<Integer> getFemaleAthleteIds();
+    int updateAthleteId(int id, int athleteId);
 }
--- src/main/java/com/github/pbkhyglszy/gymnastics_manager/service/SystemStatusService.java
+++ src/main/java/com/github/pbkhyglszy/gymnastics_manager/service/SystemStatusService.java
@@ -1,8 +1,49 @@
 package com.github.pbkhyglszy.gymnastics_manager.service;
 
+import com.github.pbkhyglszy.gymnastics_manager.entity.Athlete;
+import com.github.pbkhyglszy.gymnastics_manager.mapper.RegistrationMapper;
+import com.github.pbkhyglszy.gymnastics_manager.mapper.SystemStatusMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class SystemStatusService {
+    @Autowired
+    SystemStatusMapper systemStatusMapper;
+    @Autowired
+    RegistrationMapper registrationMapper;
+
+    public boolean openRegistration() {
+        if (systemStatusMapper.get() == 0) {
+            systemStatusMapper.update(1);
+            return true;
+        } else return false;
+    }
+
+    public boolean closeRegistration() {
+        if (systemStatusMapper.get() == 1) {
+            systemStatusMapper.update(0);
+            updateAthleteId();
+            return true;
+        } else return false;
+    }
+
+    private void updateAthleteId() {
+        List<Integer> maleAthleteIds = systemStatusMapper.getMaleAthleteIds();
+        List<Integer> femaleAthleteIds = systemStatusMapper.getFemaleAthleteIds();
+        int male = 1, female = 0;
+        for (int id : maleAthleteIds
+        ) {
+            systemStatusMapper.updateAthleteId(id, male);
+            male += 2;
+        }
+        for (int id : femaleAthleteIds
+        ) {
+            systemStatusMapper.updateAthleteId(id, female);
+            female += 2;
+        }
+    }
 
 }
